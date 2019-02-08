a7cbff8b6562509b9aa47740a78ebc6a621a7db3
alex14v <alexv14vvv@gmail.com>
2019-02-08 15:20:21 +0700

    fix

--- src/migrations/2017_05_21_000000_create_accounting_ledgers_table.php
+++ src/migrations/2017_05_21_000000_create_accounting_ledgers_table.php
@@ -3,7 +3,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateAccountingLedgersTable extends Migration
+class ModifyAjtTableAddJson extends Migration
 {
     /**
      * Run the migrations.
